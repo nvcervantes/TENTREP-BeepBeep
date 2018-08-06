d437728132a3ae9a087adf4bc8933b5c48647562
Shierene Cervantes <shierenecervantes23@gmail.com>
2018-08-06 19:41:09 +0800

    New Project

---
+++ BeepBeepBeep/app/src/main/java/apc/edu/ph/beepbeepbeep/Login.java
@@ -0,0 +1,20 @@
+package apc.edu.ph.beepbeepbeep;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View v) {
+        startActivity(new Intent(Login.this, MainActivity.class));
+    }
+
+}
